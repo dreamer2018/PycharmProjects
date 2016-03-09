b9e3ad14aaacd45b764ab8dd6108bbe00c503e7e
dreamer2018 <zhoupans_mail@163.com>
2016-03-09 09:10:31 +0800

    add

---
+++ HomeWork/sort.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+__author__ = 'zhoupan'
+
+if __name__ == '__main__':
+    list=[2,5,1,3,8,9,6,7]
+    list.sort(reverse=True)
+    print(list)
+    list.sort()
+    print(list)
