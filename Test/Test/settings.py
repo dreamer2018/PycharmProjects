68bb4799aad4a03805c36e1c1d674fab83a65723
dreamer2018 <zhoupans_mail@163.com>
2016-03-29 18:22:55 +0800

    pymysql

--- Test/Test/settings.py
+++ Test/Test/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'Test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'zhoupan',
+        'USER':'root',
+        'PASSWORD':'zhoupan',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
