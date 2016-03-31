282a00c4bf9ffc9667c02be451490414cf8923a2
dreamer2018 <zhoupans_mail@163.com>
2016-03-31 14:09:52 +0800

    add

--- HomeWork/DataBase.py
+++ HomeWork/DataBase.py
@@ -2,5 +2,16 @@
 #coding=utf-8
 __author__ = 'zhoupan'
 
-
-
+import MySQLdb
+try:
+    connect=MySQLdb.connect(host="localhost",user="root",passwd="zhoupan")
+    connect.select_db('zhoupan')
+    cur=connect.cursor()
+    cur.execute('show tables;')
+    result=cur.fetchall()
+    for i in result:
+        print i
+    cur.close()
+    connect.close()
+except MySQLdb.Error,e:
+    print "mysql Error %d:%s" % (e.args[0],e.args[1])
