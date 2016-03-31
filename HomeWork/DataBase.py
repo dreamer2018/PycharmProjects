d8b6004532070ee056dab1fb0daf507aa47b0e42
dreamer2018 <zhoupans_mail@163.com>
2016-03-31 14:25:54 +0800

    add

--- HomeWork/DataBase.py
+++ HomeWork/DataBase.py
@@ -7,10 +7,8 @@ try:
     connect=MySQLdb.connect(host="localhost",user="root",passwd="zhoupan")
     connect.select_db('zhoupan')
     cur=connect.cursor()
-    cur.execute('show tables;')
-    result=cur.fetchall()
-    for i in result:
-        print i
+    cur.execute("create table test ( id int primary key auto_increment, name varchar(20) not null , age tinyint default 0,sex boolean default true);")
+
     cur.close()
     connect.close()
 except MySQLdb.Error,e:
