14dc5dd3f2e64a02526a30fba7ae098953a65357
dreamer2018 <zhoupans_mail@163.com>
2016-03-31 21:35:14 +0800

    add

--- HomeWork/DataBase.py
+++ HomeWork/DataBase.py
@@ -1,15 +1,40 @@
 #!/usr/bin/env python
 #coding=utf-8
+
 __author__ = 'zhoupan'
 
+import random
 import MySQLdb
-try:
-    connect=MySQLdb.connect(host="localhost",user="root",passwd="zhoupan")
-    connect.select_db('zhoupan')
-    cur=connect.cursor()
-    cur.execute("create table test ( id int primary key auto_increment, name varchar(20) not null , age tinyint default 0,sex boolean default true);")
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
-    cur.close()
-    connect.close()
-except MySQLdb.Error,e:
-    print "mysql Error %d:%s" % (e.args[0],e.args[1])
+class RandomChar():
+  """用于随机生成汉字"""
+  @staticmethod
+  def Unicode():
+    val = random.randint(0x4E00, 0x9FBF)
+    return unichr(val)
+
+  @staticmethod
+  def GB2312():
+    head = random.randint(0xB0, 0xCF)
+    body = random.randint(0xA, 0xF)
+    tail = random.randint(0, 0xF)
+    val = ( head << 8 ) | (body << 4) | tail
+    str = "%x" % val
+    return str.decode('hex').decode('gb2312')
+  @staticmethod
+  def GetRandom():
+    val = random.randint(1,1000000)
+    rtu=val%101
+    if(val%2 == 0):
+      return True,rtu
+    else:
+      return False,rtu
+if __name__ == '__main__':
+    f=open('1.sql','a+')
+    for j in range(1,10000):
+        sex,age=RandomChar.GetRandom()
+        sql='insert into test (name,age,sex) values ("'+RandomChar.GB2312()+RandomChar.GB2312()+RandomChar.GB2312()+'",'+str(age)+","+str(sex)+");"
+        print sql
