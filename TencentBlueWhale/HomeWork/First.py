324cc9030ee143e6acd92db40e126aca90a4e6ea
dreamer2018 <zhoupans_mail@163.com>
2016-04-11 21:10:33 +0800

    add

--- TencentBlueWhale/HomeWork/First.py
+++ TencentBlueWhale/HomeWork/First.py
@@ -1,32 +1,53 @@
 #!/usr/bin/env python
 #coding=utf-8
+'''
+#第一题：
 __author__ = 'zhoupan'
 print repr(u'中国')
 
-
+#第二题：
 #通过lambda函数实现两个数相乘
 multiply = lambda a,b : a*b
 a=10
 b=20
 print multiply(a,b)
 
-import xlwd
-file = xlwd.open_workbook('/c/Users/Administrator/PycharmProjects/TencentBlueWhale/HomeWork/')
+#第三题：
+import xlwt
 
-a[0]=["1","张三",150,120,100]
-a[1]=["2","李四",90,99,95]
-a[2]=["3","王五",60,66,68]
-print a[2][1]
+workbook=xlwt.Workbook(encoding='utf-8')
+booksheet=workbook.add_sheet('student')
 
-for i in range(0,3):
-    for j in range(0,5):
-        row=i
-        col=j
-        # 类型 0 empty,1 string, 2 number, 3 date, 4 boolean, 5 error
-        if(j<=2):
-            type=1
-        else:
-            type=2
-        xf=0
-        value=a[i][j]
-        file.put_cell(row,col,type,value,xf)
+Data=(("1","张三",150,120,100),("2","李四",90,99,95),("3","王五",60,66,68))
+
+for i,row in enumerate(Data):
+    for j,col in enumerate(row):
+        booksheet.write(i,j,col)
+
+workbook.save('student.xls')
+
+#第四题：
+def deco(func):
+    if func.__doc__ == None :
+        print func.__name__,'not doc'
+    else:
+        print func.__name__,'doc is:',func.__doc__
+    return func
+@deco
+def func1():
+    'a test function'
+    print 'func1 was executed'
+@deco
+def func2():
+    print 'func2 was executed'
+func1()
+func2()
+
+
+
+if 1 in [1,0] == True:
+    print 'a'
+else:
+    print 'b'
+'''
+#第五题
