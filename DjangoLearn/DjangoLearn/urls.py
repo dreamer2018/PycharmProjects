08406e975ef83b05aef81842f8a37f566ab1d671
dreamer2018 <zhoupans_mail@163.com>
2016-08-08 10:07:03 +0800

    表单方法练习

--- DjangoLearn/DjangoLearn/urls.py
+++ DjangoLearn/DjangoLearn/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import *
-from DjangoLearn.view import *
 from django.contrib import admin
 
+from DjangoLearn.view import *
+from books import views
+
 urlpatterns = [
-    url(r'^$',index),url(r'^hello/$',hello),url(r'^time/(\d{1,2})/$',hour_ahead),url(r'^temp/$',temp_time),url(r'^simple/$',temp_simp),
-    url(r'^local_test/$',local_test),url(r'^my/$',my),url(r'^admin/',include(admin.site.urls))
+    url(r'^$', index), url(r'^hello/$', hello), url(r'^time/(\d{1,2})/$', hour_ahead), url(r'^temp/$', temp_time),
+    url(r'^simple/$', temp_simp),
+    url(r'^local_test/$', local_test), url(r'^my/$', my),
+    url(r'^admin/', include(admin.site.urls)), url(r'^search-form/$', views.search_form),url(r'^search/$',views.search)
 ]
